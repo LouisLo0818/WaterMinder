02936c9e36006cdda2b0573620aee89366779c54
lokyinjason <lokyinjason921@gmail.com>
2023-11-30 19:39:52 +0800

    Day & Cup Struct

--- WaterMinder/ContentView.swift
+++ WaterMinder/ContentView.swift
@@ -8,6 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("dayList") var dayListData: Data = Data()
+    @AppStorage("cupList") var cupListData: Data = Data()
+    
+    var dayList: [Day] {
+        get {
+            if let decodedData = try? JSONDecoder().decode([Day].self, from: dayListData) {
+                return decodedData
+            }
+            return []
+        }
+        set {
+            if let encodedData = try? JSONEncoder().encode(newValue) {
+                dayListData = encodedData
+            }
+        }
+    }
+    var cupList: [Cup] {
+        get {
+            if let decodedData = try? JSONDecoder().decode([Cup].self, from: cupListData) {
+                return decodedData
+            }
+            return []
+        }
+        set {
+            if let encodedData = try? JSONEncoder().encode(newValue) {
+                cupListData = encodedData
+            }
+        }
+    }
+    
     var body: some View {
         TabView {
             HydrationView()
@@ -33,4 +63,18 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Day: Codable {
+    var goal: Double;
+    var amount: Double;
+    var date: Date = Date.now
+    var cups: [Cup]
+}
+
+struct Cup: Codable {
+    var name: String;
+    var type: String;
+    var hydrationImpact: Double;
+    var amount: Double;
 }
